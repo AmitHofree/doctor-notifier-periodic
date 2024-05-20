f55f2c69af26df68d5e0349b4f19ca76a4bddc85
Amit Hofree <amit.hofree@gmail.com>
2024-05-20 08:38:46 -0700

    Initial version

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,42 @@
-/**
- * Welcome to Cloudflare Workers!
- *
- * This is a template for a Scheduled Worker: a Worker that can run on a
- * configurable interval:
- * https://developers.cloudflare.com/workers/platform/triggers/cron-triggers/
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Run `curl "http://localhost:8787/__scheduled?cron=*+*+*+*+*"` to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import {Service} from '@cloudflare/workers-types';
+import { WorkerEntrypoint } from 'cloudflare:workers';
+
+type NotificationsRegisteredRow = {
+	item_key_index: string
+};
+
+interface DoctorNotifierService extends WorkerEntrypoint {
+	checkAndNotify: (itemKeyIndex: string) => undefined;
+}
+
+interface Env {
+	DB: D1Database;
+	SERVICE: Service<DoctorNotifierService>;
+}
 
 export default {
-	// The scheduled handler is invoked at the interval set in our wrangler.toml's
-	// [[triggers]] configuration.
-	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext): Promise<void> {
-		// A Cron Trigger can make requests to other endpoints on the Internet,
-		// publish to a Queue, query a D1 Database, and much more.
-		//
-		// We'll keep it simple and make an API call to a Cloudflare API:
-		let resp = await fetch('https://api.cloudflare.com/client/v4/ips');
-		let wasSuccessful = resp.ok ? 'success' : 'fail';
-
-		// You could store this result in KV, write to a D1 Database, or publish to a Queue.
-		// In this template, we'll just log the result:
-		console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
-	},
+	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext) {
+		console.log("Triggered periodic update");
+		const itemKeyIndexes = await getAllRegisteredItemKeyIndexes(env);
+		itemKeyIndexes.forEach(itemKeyIndex => {
+			console.log(`Triggering update for itemKeyIndex ${itemKeyIndex}`);
+			env.SERVICE.checkAndNotify(itemKeyIndex);
+		});
+	}
 };
+
+async function getAllRegisteredItemKeyIndexes(env: Env): Promise<string[]> {
+	try {
+		const stmt = env.DB.prepare("SELECT DISTINCT item_key_index FROM notifications_registered");
+		const { results, success } = await stmt.all<NotificationsRegisteredRow>();
+		if (!success) {
+			console.log("Unknown error executing SQL query in getAllRegisteredItemKeyIndexes");
+			return [];
+		}
+		return results.map(result => result.item_key_index)
+	} catch (e) {
+		if (e instanceof Error)
+			console.error(`Error executing SQL query in getAllRegisteredItemKeyIndexes - ${e.message}`);
+		return [];
+	}
+}
